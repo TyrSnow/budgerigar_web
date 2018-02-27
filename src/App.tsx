c6f99d7fe6d4aaecc59ca5af796a540a85391c45
TianYu <1127114376@qq.com>
2018-02-27 23:09:20 +0800

    init route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
-import './App.css';
+import { Router, Switch, Route } from 'react-router';
+import { createBrowserHistory } from 'history';
 
-const logo = require('./logo.svg');
+import Index from './index/index';
+import Login from './login/index';
+
+const history = createBrowserHistory();
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Router history={history}>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Index} />
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/login/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="App">登陆页面</div>
+    );
+  }
+}
+
+export default Login;
